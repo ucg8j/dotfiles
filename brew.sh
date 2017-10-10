7b1d9d0e60fb10537bcc7efb25f8b7e5fb4b6ff4
ucg8j <lukesingham@gmail.com>
2017-10-10 12:52:12 +0100

    Add licecap - gif screen capture

--- brew.sh
+++ brew.sh
@@ -155,6 +155,7 @@ brew cask install --appdir="~/Applications" virtualbox
 brew cask install --appdir="~/Applications" vagrant
 brew cask install --appdir="~/Applications" vagrant-manager
 brew cask install --appdir="~/Applications" pycharm-ce 
+brew cask install --appdir="~/Applications" licecap 
 
 # Install java
 brew cask install java 
